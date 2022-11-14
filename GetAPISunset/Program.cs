5b81c2143992efe1a47d61a2e358f7146ca35f27
Lars <lars.asplund@hotmail.com>
2022-11-14 08:39:42 +0100

    Add project files.

---
+++ GetAPISunset/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using GetAPISunset.Services;
+using GetAPISunset.Models;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace GetAPISunset
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            double latitude = 60.67452;
+            double longitude = 17.14174;
+            var _cityClient = new CityNameClient();
+            var resultFromCity = await _cityClient.GetCityAsync(latitude, longitude);
+            Console.WriteLine(resultFromCity.city.ToString());
+        }
+    }
+}
---
+++ GetAPISunset/Services/CityNameClient.cs
@@ -0,0 +1,29 @@
+﻿using GetAPISunset.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAPISunset.Services
+{
+    public class CityNameClient
+    {
+        public async Task<City> GetCityAsync(double lat, double lon)
+        {
+            var _client = new HttpClient();
+            string cityUrl = $"https://api.bigdatacloud.net/data/reverse-geocode-client?latitude={lat}&longitude={lon}&localityLanguage=en";
+            var response = await _client.GetAsync(cityUrl);
+            response.EnsureSuccessStatusCode();
+
+            City cn = await response.Content.ReadFromJsonAsync<City>();
+            var city = new City()
+            {
+                city = cn.city
+            };
+            return city;
+        }
+    }
+}
